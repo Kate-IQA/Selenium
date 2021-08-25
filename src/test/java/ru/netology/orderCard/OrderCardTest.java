351feebf5655417215977e499457e14f25b119e6
Kate-IQA <81325024+Kate-IQA@users.noreply.github.com>
2021-08-25 22:16:32 +0300

    Update OrderCardTest.java
    update negative tests again

--- src/test/java/ru/netology/orderCard/OrderCardTest.java
+++ src/test/java/ru/netology/orderCard/OrderCardTest.java
@@ -62,11 +62,12 @@ public class OrderCardTest {
 
     @Test
     void shouldNotFillName() {
+        driver.findElement(By.cssSelector("[data-test-id=name] .input__control")).sendKeys("");
         driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("+79139469364");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
         String expected = "Поле обязательно для заполнения";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=phone] .input_invalid .input__sub")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=name] .input_invalid .input__sub")).getText().trim();
         assertEquals(expected, actual);
     }
 
@@ -98,7 +99,7 @@ public class OrderCardTest {
         driver.findElement(By.cssSelector("[data-test-id=phone] .input__control")).sendKeys("+79139469364");
         driver.findElement(By.cssSelector("button")).click();
         String expected = "Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй";
-        String actual = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid. checkbox__text")).getText().trim();
         assertEquals(expected, actual);
 
     }
